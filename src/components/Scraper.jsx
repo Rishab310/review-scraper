c0409666768c2bacdf5bac2d069ec5eb3f6e4911
Rishab Goyal <rishabgoyal432@gmail.com>
2021-06-01 21:37:03 +0530

    Initial

---
+++ src/components/Scraper.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+class Scraper extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <br /><br /><br />
+        <div className="container">
+          <div className="row">
+            <h1 className="primary-heading font-weight-bold text-center">Amazon Review Scraper</h1>
+          </div>
+          <div className="row d-flex justify-content-center align-items-center mt-5">
+            <div className="col-12 col-md-2 mb-4">
+              <label for="url" className="form-label text-light m-0">Enter Product URL : </label>
+            </div>
+            <div className="col-12 col-md-4 mb-4">
+              <input type="text" className="form-control" id="url"  />
+            </div>
+          </div>
+          <div className="row d-flex justify-content-center align-items-center">
+            <div className="col-12 col-md-2 mb-4">
+              <label for="pages" className="form-label text-light m-0">Enter No. Pages : </label>
+            </div>
+            <div className="col-12 col-md-4 mb-4">
+              <input type="text" className="form-control" id="pages" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Scraper;
+
+// https://www.youtube.com/watch?v=D9xiIZAPZUI
